bc9321c8608d5d9a680161d079de376661f2ad4d
Filip Pollum <filip.pollum@gmail.com>
2024-05-12 10:57:07 +0200

    postawowy css i dodanie fake api

---
+++ nuxtHairdresserF/nuxtHairdresser/layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+        <header class="shadow-sm bg-white">
+            <nav class="container mx-auto p-4 flex justify-between">
+                <Nuxt-link to="/" class="font-bold" >Nuxt HAIR</Nuxt-link>
+                <ul class="flex gap-4">
+                    <li><NuxtLink to="/" class="btn">Home</NuxtLink></li>
+                    <li><NuxtLink to="/about" class="btn">About</NuxtLink></li>
+                </ul>
+            </nav>
+        </header>
+        <div class="container mx-auto p-4">
+            <slot/>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.router-link-exact-active{
+    color: rgba(240, 183, 13, 0.9);
+}
+</style>
---
+++ nuxtHairdresserF/nuxtHairdresser/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo ad officiis fugit fuga recusandae dicta dolor nobis cupiditate. Magni dicta excepturi odit perferendis maxime voluptates id, distinctio eligendi eveniet laboriosam.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta repellendus minima molestiae suscipit tenetur voluptate, veniam dolorum quaerat accusamus ex dolore quisquam officia voluptatem nulla quod sequi eum velit qui!</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+<style  scoped>
+h2{
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+p{
+    margin: 20px 0;
+
+}
+</style>
