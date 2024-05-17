497cd3a44fe051aad5bccdbb822f1a8d97d0be5b
00DZnkbl00 <dawidzdrojewski12345678@gmail.com>
2024-05-17 02:49:40 +0200

    models relations

--- pythonHairdresserB/hairdresserBackend/reservationApp/admin.py
+++ pythonHairdresserB/hairdresserBackend/reservationApp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Worker,Client,Service,Order,Session
+from .models import Worker, Client, Service, Order, Session
+
 # Register your models here.
 admin.site.register(Worker)
 admin.site.register(Client)
--- pythonHairdresserB/hairdresserBackend/reservationApp/models.py
+++ pythonHairdresserB/hairdresserBackend/reservationApp/models.py
@@ -21,26 +21,30 @@ class Client(models.Model):
         return f"{self.name} {self.surname}"
 
 
-class Order(models.Model):
-    code = models.CharField(max_length=32)
-    paymentState = models.BooleanField()
-    created = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.code
-
-
-class Session(models.Model):
-    date = models.DateField(null=True)
-    time = models.TimeField(validators=[validate_time_interval], null=True)
-
-    def __str__(self):
-        return f"{self.date} {self.time}"
-
-
 class Service(models.Model):
     name = models.CharField(max_length=16)
     price = models.FloatField()
 
     def __str__(self):
         return self.name
+
+
+class Order(models.Model):
+    code = models.CharField(max_length=32)
+    paymentState = models.BooleanField()
+    created = models.DateTimeField(auto_now_add=True)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.code
+
+
+class Session(models.Model):
+    date = models.DateField()
+    time = models.TimeField(validators=[validate_time_interval])
+    worker = models.ForeignKey(Worker, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.date} {self.time}"
